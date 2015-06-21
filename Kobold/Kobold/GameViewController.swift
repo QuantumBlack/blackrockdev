35b9cc9b3d1e93d7d613598816e5e7da5772b703
Connor Bergmann <Connor@Connors-MBP.home>
2015-06-20 19:30:53 -0700

    assdasd

---
+++ Kobold/Kobold/GameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GameViewController.swift
+//  Kobold
+//
+//  Created by Connor Bergmann on 6/20/15.
+//  Copyright (c) 2015 blackrockdev. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
