c6172d94595e60221a6b54869fd9944be8079d55
Connor Bergmann <Connor@Connors-MBP.home>
2015-06-20 18:16:32 -0700

    added project kobold

---
+++ kobold/koboldTests/koboldTests.swift
@@ -0,0 +1,36 @@
+//
+//  koboldTests.swift
+//  koboldTests
+//
+//  Created by Connor Bergmann on 6/20/15.
+//  Copyright (c) 2015 blackrockdev. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class koboldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
